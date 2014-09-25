e93542f9fe2b1454922f684647a64f5f95e54158
brunty <openblue555@gmail.com>
2014-09-25 13:28:30 +0100

    New version of raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,24 +2,28 @@ package raindrops
 
 import "strconv"
 
+// primeFactors for us to use to determine what to output
+var primeFactors = []struct {
+	prime   int
+	message string
+}{
+	{3, "Pling"},
+	{5, "Plang"},
+	{7, "Plong"},
+}
+
 // Convert gives us the raindrops for a given number as per the rules
-func Convert(number int) string {
-	var returnString string
+func Convert(input int) string {
+	var output string
 
-	if number%3 == 0 {
-		returnString += "Pling"
+	for _, factor := range primeFactors {
+		if input%factor.prime == 0 {
+			output += factor.message
+		}
 	}
 
-	if number%5 == 0 {
-		returnString += "Plang"
+	if output == "" {
+		return strconv.Itoa(input)
 	}
-
-	if number%7 == 0 {
-		returnString += "Plong"
-	}
-
-	if returnString == "" {
-		return strconv.Itoa(number)
-	}
-	return returnString
+	return output
 }
